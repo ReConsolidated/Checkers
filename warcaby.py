099d883d7ed5f4f6191723043cadb7bb18654c12
ReConsolidated <gracjanpasik@gmail.com>
2017-10-18 23:03:18 +0200

    Add files via upload

--- warcaby.py
+++ warcaby.py
@@ -1,5 +1,7 @@
 import sys, pygame
 from pole import Field
+from pole import get_moves
+import numpy
 
 
 FIELD_COLOR = (0,0,0)
@@ -8,43 +10,87 @@ FPS = 100 # not higher than 1000, don't change without a good reason
 pygame.init()
 
 size = width, height = 900, 600
+fields = numpy.array([
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()],
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()],
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()],
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()],
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()],
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()],
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()],
+                        [Field(),Field(),Field(),Field(),Field(),Field(),Field(),Field()]
+                     ])
 
-fields = [[Field() for j in range(8)] for i in range(8)]
 num = 0
 ex = 0
 ey = 0
+
 for tab in fields:
     for one in tab:
-        if num%2 == 1:
-            one.makeBlank()
-        if ey%2 == 1:
-            one.set_position_and_make_50(ex, ey)
-        else:
-            one.set_position_and_make_100(ex, ey)
-        ex = ex + 1
-        num = num + 1
-    ey = ey + 1
-    ex = 0
+        one.position = [ex, ey]
+        one.createfield(ex, ey)
+        if (ex+ey)%2 == 1:
+            one.isActive = 0
+        if ey < 2:
+            one.possesed = 2
+        if ey > 1:
+            one.possesed = 0
+        if ey > 5:
+            one.possesed = 1
+        ex+=1
+    ey+=1
+    ex=0
 
 
 
 screen = pygame.display.set_mode(size)
 
 timer = 1
-
+moves = []
+checked = []
+n = 0
 while 1:
     if pygame.time.get_ticks()%(1000/FPS) == 0 and pygame.time.get_ticks() is not timer:
         timer = pygame.time.get_ticks()
+        mouse_pos = pygame.mouse.get_pos()
+
+
         for event in pygame.event.get():
             if event.type == pygame.QUIT: sys.exit()
+            if event.type == pygame.MOUSEBUTTONDOWN:
+                for tab in fields:
+                    for one in tab:
+                        if one.collides(mouse_pos):
+                            if moves:
+                                for move in moves:
+                                    if one.position[0] == move[1] and one.position[1] == move[0]:
+                                        fields[move[2]][move[3]].possesed = 0
+                                        one.possesed = fields[checked[1]][checked[0]].possesed
+                                        fields[checked[1]][checked[0]].possesed = 0
+                                        moves = []
+                                        n = 1
+                                    else:
+                                        n = 0
+                            else:
+                                n=0
+                            if n == 0:
+                                moves = get_moves(fields, one.position[0], one.position[1])
+                                checked = one.position
+
 
         screen.fill((255,255,255))
-        mouse_pos = pygame.mouse.get_pos()
-        for i in range(8):
-            for j in range(8):
-                if fields[i][j].isActive:
-                    if fields[i][j].collides(mouse_pos):
-                        pygame.draw.rect(screen, (50,50,50), fields[i][j].rect)
-                    else:
-                        pygame.draw.rect(screen, (0,0,0), fields[i][j].rect)
+        for tab in fields:
+            for one in tab:
+                if one.isActive:
+                    pygame.draw.rect(screen, (0,0,0), one.rect)
+                    if one.possesed == 1:
+                        pygame.draw.circle(screen, (255,255,255), one.circle_pos, 15)
+                    if one.possesed == 2:
+                        pygame.draw.circle(screen, (71,36,0), one.circle_pos, 15)
+                for move in moves:
+                    if one.position[0] == move[1] and one.position[1] == move[0]:
+                        pygame.draw.rect(screen, (0,255,0), one.rect)
+
+
+
         pygame.display.flip()
